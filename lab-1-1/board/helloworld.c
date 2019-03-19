f86b067c908562e85d0130d9624a0db0d0e085f9
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-03-19 12:48:55 +0000

    WORKING VERSION ON THE BOARD

--- lab-1-1/board/helloworld.c
+++ lab-1-1/board/helloworld.c
@@ -7,104 +7,120 @@
 
 #include "helloworld.h"
 
-uint8_t octetstr_rd( char* r, uint8_t n_r );
-void octetstr_wr( const char* x, uint8_t n_x );
-void reverse_array( char *array, uint8_t n );
+uint8_t octetstr_rd( uint8_t* r, uint8_t n_r );
+void octetstr_wr( const uint8_t* x, uint8_t n_x );
+void reverse_array( uint8_t *array, uint8_t n );
+void my_print(char *string)  {
+  int len = strlen(string);
+  for(int i = 0; i < len; i++)  {
+    scale_uart_wr( SCALE_UART_MODE_BLOCKING, string[ i ] );
+  }
+}
 
 uint8_t hex_to_int(char c);
+uint8_t hex_to_int2(char c0, char c1);
 char int_to_hex(uint8_t i);
+void int_to_hex2(char* hex_chars, uint8_t i);
 
 int main( int argc, char* argv[] ) {
   // initialise the development board, using the default configuration
   if( !scale_init( &SCALE_CONF ) ) {
     return -1;
   }
+  uint8_t read_buf[128] = {0};
 
-  // char x[] = "hello world";
-
-  // while( true ) {
-    // read  the GPI     pin, and hence switch : t   <- GPI
-    bool t = scale_gpio_rd( SCALE_GPIO_PIN_GPI        );
-    // write the GPO     pin, and hence LED    : GPO <- t
-             scale_gpio_wr( SCALE_GPIO_PIN_GPO, t     );
-
-    // write the trigger pin, and hence LED    : TRG <- 1 (positive edge)
-             scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
-    // delay for 500 ms = 1/2 s
-    scale_delay_ms( 500 );
-    // write the trigger pin, and hence LED    : TRG <- 0 (negative edge)
-             scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
-    // delay for 500 ms = 1/2 s
-    scale_delay_ms( 500 );
+  while( true ) {
+    // my_print("Enter Octet String: ");
+    
+    scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
 
 
 
-    char read_buf[128] = {0};
     uint8_t n = octetstr_rd(read_buf, 128);
-    // reverse_array(read_buf, n);
+    reverse_array(read_buf, n);
     octetstr_wr(read_buf, n);
 
     
-    // int n = strlen( x );
+    // my_print( "\x0D\x0A" );
 
-  //   // write x = "hello world" to the UART
-  //   for( int i = 0; i < n; i++ ) {
-  //     scale_uart_wr( SCALE_UART_MODE_BLOCKING, x[ i ] );
-  //   }
-  // }
+    scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
+
+  }
 
   return 0;
 }
 
 
-uint8_t octetstr_rd(char* r, uint8_t n_r)  {
+uint8_t octetstr_rd(uint8_t* r, uint8_t n_r)  {
   // Reads first two characters, these are hex characters
+  char c0 = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
   char c1 = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-  char c2 = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
   // Convert ASCII char to integer
-  uint8_t n1 = hex_to_int(c1);
-  uint8_t n2 = hex_to_int(c2);
-  // First char is an order larger than second, so multiply by 16
-  n1*=16;
-  uint8_t n = n1+n2;
+  uint8_t n = hex_to_int2(c0, c1);
   //Colon
-  //Read n bytes after colon
   scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-  for (uint8_t i = 0; i < 2*n; i++)  { //Iterate by 2 each time, as each 2 characters represents a byte
-    r[i] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+
+  //Read n bytes after colon
+  for (uint8_t i = 0; i < n; i++)  { //Iterate by 2 each time, as each 2 characters represents a byte
+    c0 = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+    c1 = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+
+    uint8_t byte = hex_to_int2(c0, c1);
+
+    r[i] = byte;
   }
+
+  scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  // scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+
   return n;
 }
 
-void octetstr_wr( const char* x, uint8_t n_x )  {
-  uint8_t n1 = n_x / 16;
-  uint8_t n2 = n_x % 16;
-  char c1 = int_to_hex(n1);
-  char c2 = int_to_hex(n2);
+void octetstr_wr( const uint8_t* x, uint8_t n_x )  {
+  char hex_chars[2];
+
+  int_to_hex2(hex_chars, n_x);
 
   //Print n bytes and a colon to UART
-  scale_uart_wr(SCALE_UART_MODE_BLOCKING, c1);
-  scale_uart_wr(SCALE_UART_MODE_BLOCKING, c2);
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, hex_chars[0]);
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, hex_chars[1]);
   scale_uart_wr(SCALE_UART_MODE_BLOCKING, 58);
 
   //Print string
-  for (uint8_t i = 0; i < 2*n_x; i++)  {
-    scale_uart_wr(SCALE_UART_MODE_BLOCKING, x[i]);
+  for (uint8_t i = 0; i < n_x; i++)  {
+    int_to_hex2(hex_chars, x[i]);
+    scale_uart_wr(SCALE_UART_MODE_BLOCKING, hex_chars[0]);
+    scale_uart_wr(SCALE_UART_MODE_BLOCKING, hex_chars[1]);
+  }
+  my_print( "\x0D" );
+  // my_print( "\x0D\x0A" );
+
+}
+
+void reverse_array( uint8_t *array , uint8_t n)  {
+  char swp;
+  for (uint8_t i = 0; i < n/2; i++)  {
+    swp = array[i];
+    array[i] = array[n-i-1];
+    array[n-i-1] = swp;
   }
 }
 
-void reverse_array( char *array , uint8_t n)  {
-  char swp0, swp1;
-  for (uint8_t i = 0; i < (2*n)/2; i+=2)  {
-    swp0 = array[i];
-    swp1 = array[i+1];
+uint8_t hex_to_int2(char c0, char c1)  {
+  uint8_t n1 = hex_to_int(c0);
+  uint8_t n2 = hex_to_int(c1);
+  // First char is an order larger than second, so multiply by 16
+  n1*=16;
+  return n1+n2;
+}
 
-    array[i]   = array[2*n-(i+2)];
-    array[i+1] = array[2*n-(i+1)];
+void int_to_hex2(char* hex_chars, uint8_t i)  {
+  uint8_t i0 = i / 16;
+  uint8_t i1 = i % 16;
 
-    array[2*n-(i+2)] = swp0;
-    array[2*n-(i+1)] = swp1;
-  }
+  hex_chars[0] = int_to_hex(i0);
+  hex_chars[1] = int_to_hex(i1);
+
 }
 
 uint8_t hex_to_int(char c)  {
