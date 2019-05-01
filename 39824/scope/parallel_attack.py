36afdc2cb0bbc0c4ae59c9413ce5b00aa7b7067c
Harry Waugh <hw16471@my.bristol.ac.uk>
2019-05-01 09:14:22 +0100

    Set makefile options for attacking board and file

--- 39824/scope/parallel_attack.py
+++ 39824/scope/parallel_attack.py
@@ -21,46 +21,35 @@ def scope_adc2volts( range, x ) :
 def scope_volts2adc( range, x ) :
     return ( float( x ) * float( scope_adc_max ) ) / range;
 
-## Convert a string (e.g., string, or bytearray) into a list (or sequence).
-##
-## \param[in] x  a  string
-## \return       a  list   r st. r[ i ] = ord( x[ i ] )
-
 def str2seq( x ) :
     return          [ ord( t ) for t in x ]
-
-## Convert a list (or sequence) into a string (e.g., string, or bytearray).
-##
-## \param[in] x  a  list
-## \return       a  string r st. r[ i ] = chr( x[ i ] )
 
 def seq2str( x ) :
     return ''.join( [ chr( t ) for t in x ] )
 
-## Open  (or start)  communication with SCALE development board.
-##
-## \return    fd a communication end-point
+
+def str2octetstr( x ) :
+  return ( '%02X' % ( len( x ) ) ) + ':' + ( binascii.b2a_hex( x ) )
+
+def octetstr2str( x ) :
+  t = x.split( ':' ) ; n = int( t[ 0 ], 16 ) ; x = binascii.a2b_hex( t[ 1 ] )
+
+  if( n != len( x ) ) :
+    raise ValueError
+  else :
+    return x
 
 def board_open() :
-    # if   ( args.mode == 'uart'   ) :
+  if   ( args.mode == 'uart'   ) :
     fd = serial.Serial( port = args.uart, baudrate = 9600, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, timeout = None )
-    # elif ( args.mode == 'socket' ) :
-        # fd = socket.socket( socket.AF_INET, socket.SOCK_STREAM ) ; fd.connect( ( args.host, args.port ) ) ; fd = fd.makefile( mode = 'rwb', bufsize = 1024 )
+  elif ( args.mode == 'socket' ) :
+    fd = socket.socket( socket.AF_INET, socket.SOCK_STREAM ) ; fd.connect( ( args.host, args.port ) ) ; fd = fd.makefile( mode = 'rwb', bufsize = 1024 )
 
-    return fd
-
-## Close (or finish) communication with SCALE development board.
-##
-## \param[in] fd a communication end-point
+  return fd
 
 def board_close( fd ) :
     fd.close()
 
-## Read  (or recieve) a string from SCALE development board, automatically
-## managing CR-only EOL semantics.
-##
-## \param[in] fd a communication end-point
-## \return    r  a string (e.g., string, or bytearray)
 
 def board_rdln( fd    ) :
     r = ''
@@ -72,39 +61,11 @@ def board_rdln( fd    ) :
             break
         else:
             r += t
-
     return r
 
-## Write (or send)    a string to   SCALE development board, automatically
-## managing CR-only EOL semantics.
-##
-## \param[in] fd a communication end-point
-## \param[in] x  a string (e.g., string, or bytearray)
-
 def board_wrln( fd, x ) :
-    fd.write( x + '\x0D' ) ; fd.flush()
+  fd.write( x + '\x0D' ) ; fd.flush()
 
-## Convert a length-prefixed, hexadecimal octet string into a string.
-##
-## \param[in] x  an octet string
-## \return       a  string
-## \throw        ValueError if the length prefix and data do not match
-
-def octetstr2str( x ) :
-    t = x.split( ':' ) ; n = int( t[ 0 ], 16 ) ; x = binascii.a2b_hex( t[ 1 ] )
-
-    if( n != len( x ) ) :
-        raise ValueError
-    else :
-        return x
-
-## Convert a string into a length-prefixed, hexadecimal octet string.
-##
-## \param[in] x  an octet string
-## \return       a  string
-
-def str2octetstr( x ) :
-    return ( '%02X' % ( len( x ) ) ) + ':' + ( binascii.b2a_hex( x ) )
 
 def get_traces(ntraces) :
 
@@ -308,9 +269,6 @@ def crack_aes(M, T, ntraces=1000, start_byte=0, end_byte=16, key_guess=[]):
         best_samples.append(sample)
         best_corrs.append(max_correlation)
         print("BREAKING BYTE", b)
-              
-    
-
         
     # print("\n\nKEY GUESS", key_guess[:])    
     # print("BEST_SAMPLES", best_samples)
@@ -321,11 +279,15 @@ def worker(M, T, ntraces, key_start, key_end, keys):
   crack_aes( M, T, ntraces=200, start_byte=key_start, end_byte=key_end, key_guess=keys)
 
 def attack(argc, argv):
-  print("Loading data")
+  
   ntraces = 200
-  # t, s, M, C, T = traces_ld( argv[1], ntraces=1000);
+  if(args.file == None):
+    print("Getting traces from board")
+    t, s, M, C, T = get_traces(ntraces=ntraces)
+  else:
+    print("Loading traces from file")
+    t, s, M, C, T = traces_ld( args.file, ntraces=1000);
 
-  t, s, M, C, T = get_traces(ntraces=ntraces)
 
 
 
@@ -371,7 +333,6 @@ def attack(argc, argv):
     print("\nINCORRECT KEY")
 
 
-
 if ( __name__ == '__main__' ) :
 
   
@@ -379,9 +340,13 @@ if ( __name__ == '__main__' ) :
 
 
   parser.add_argument( '--uart', dest = 'uart', type = str, action = 'store', default = '/dev/scale-board' )
+  parser.add_argument( '--mode', dest = 'mode',             action = 'store', choices = [ 'uart', 'socket' ], default = 'uart')
+  parser.add_argument( '--file', dest = 'file',             action = 'store', default = None)
+  parser.add_argument( '--host', dest = 'host', type = str, action = 'store' )
+  parser.add_argument( '--port', dest = 'port', type = int, action = 'store' )
+  parser.add_argument( '--data', dest = 'data', type = str, action = 'store' )
 
   args = parser.parse_args()
-
 
 
   attack( len( sys.argv ), sys.argv )
